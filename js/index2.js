7f3855db129d43e5337b91cc087aab3a28e05e78
arsenii-152 <55283004+arsenii-152@users.noreply.github.com>
2020-06-17 18:23:49 +0300

    ---

--- js/index2.js
+++ js/index2.js
@@ -1,8 +1,20 @@
-let controller = new ScrollMagic.Controller({vertical: false})
-let stageOne = document.querySelector('.nextp1')
-let oneFrame = anime({
-  targets: stageOne,
-  translateX: '-10vw',
-  easing: 'linear',
-  autoplay: false
+let controller = new ScrollMagic.controller()
+let ballOne = document.querySelector('.center-round2')
+let ballTwo = document.querySelector('.center-round3')
+let innerOne = document.querySelector('.inner-round2')
+let innerTwo = document.querySelector('.innner-round3')
+let firstStep = anime({
+  targets: ballOne,
+  scale: 1.5,
+  background: AC79FF,
+  duration: 1000,
+  autoplay: false,
+})
+new ScrollMagic.scene ({
+  triggerElement: ballOne,
+  triggerHook: 'onEnter'
+})
+.addTo(controller)
+.on('enter', function() {
+  firstStep.play
 })
